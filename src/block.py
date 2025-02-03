938e650d0c32a02c81a47a93751499aca55e88e6
uthmancan <uthmancan@uthmancan.uthmancan>
2025-02-03 01:27:26 +0100

    complete_project_with_comments

--- src/block.py
+++ src/block.py
@@ -1,5 +1,9 @@
 import pygame
 from position import Position
+"""
+Block class is used to control position of the blocks. 
+Usually their movement, rotation angle, color, and drawing them in the field class
+"""
 class Block:
     def __init__(self,id):
         self.id=id
@@ -9,9 +13,15 @@ class Block:
         self.rowOffset=0
         self.columnOffset=0
         self.colors=[(26,31,40),(47,230,23),(232,18,18),(226,116,17),(237,234,4),(166,0,247),(21,204,209),(13,64,216)]
+        """
+        Move function is used to move the blocks in the field by the given coordinate
+        """
     def move(self,rows, columns):
         self.rowOffset+=rows
         self.columnOffset+=columns
+    """
+    getCellPos function is used to get the position of a block
+    """
     def getCellPos(self):
         tiles = self.cells[self.rotationState]
         movedTiles=[]
@@ -19,11 +29,18 @@ class Block:
             position = Position(position.row+self.rowOffset,position.column+self.columnOffset)
             movedTiles.append(position)
         return movedTiles
+    """
+    draw function is used for drawing the block in the field using pygame.
+    Although I haven't used the pygame library in here, draw takes argument dcreen which is variable which holds pygame.display
+    """
     def draw(self,screen,offset_x,offset_y):
         tiles = self.getCellPos()
         for tile in tiles:
             tileRect = pygame.Rect(tile.column*self.cellSize+offset_x,tile.row*self.cellSize+offset_y,self.cellSize-1,self.cellSize-1)
             pygame.draw.rect(screen,self.colors[self.id],tileRect)
+    """
+    rotate function is used to rotate the blocks with the given direction to rotate which is clockwise or anticlockwise
+    """
     def rotate(self,rotation):
         if(rotation == 1):
             self.rotationState+=1
@@ -33,6 +50,9 @@ class Block:
             self.rotationState-=1
             if self.rotationState <0:
                 self.rotationState=len(self.cells)-1
+    """
+    undoRotation is used to undo the roatation if the block that should be rotated, but has a collision with a object
+    """
     def undoRotation(self,rotation):
         if(rotation == 1):
             self.rotationState-=1
--- src/field.py
+++ src/field.py
@@ -1,4 +1,8 @@
 import pygame
+"""
+Field class is used for drawing the field and dividing it to coordinates 
+because program won't move on the background, but on the front.
+"""
 class Field:
     def __init__(self,posInDispX,posInDispY,soundEffectVolume):
         self.rows=20
@@ -16,26 +20,46 @@ class Field:
             for j in range(self.columns):
                 print(self.field[i][j],end="")
             print()
+    """
+    isInside function is used for checking if the object is in the field
+    """
     def isInside(self,row,column):
         if row>=0 and row<self.rows and column>=0 and column<self.columns:
             return True
         return False
+    """
+    isEmpty function is used for checking if the particular cell in the filed is free
+    """
     def isEmpty(self,row ,column):
         if self.field[row][column]==0:
             return True
         return False
+    """
+    isRowFull function is used for checking if the row is full.
+    This function is used during the clearing of the full row and adding a point.
+    """
     def isRowFull(self,row):
         for col in range(self.columns):
             if self.field[row][col] == 0:
                 return False
         return True
+    """
+    clearRow function is used for clearing the full rows.
+    """
     def clearRow(self,row):
         for col in range(self.columns):
             self.field[row][col] =0
+    """
+    moveRowDown function is used for moving the row down after clearing the full rows
+    """
     def moveRowDown(self,row, numRows):
         for col in range(self.columns):
             self.field[row+numRows][col]=self.field[row][col]
             self.field[row][col]=0
+    """
+    clearFullRows function is the main function used for clearing the full rows.
+    This function checks the full rows and clears it and moves the rows down
+    """
     def clearFullRows(self):
         completed=0
         for i in range(self.rows-1,0,-1):
@@ -46,12 +70,19 @@ class Field:
             elif completed>0:
                 self.moveRowDown(i,completed)
         return completed
+    """
+    draw function is used to draw the agme field.
+    """
     def draw(self,screen):
         for i in range(self.rows):
             for j in range(self.columns):
                 cellNum = self.field[i][j]
                 cellRect = pygame.Rect(j*self.cellSize+self.posInDispX+1,i*self.cellSize+self.posInDispY+1,self.cellSize-1,self.cellSize-1)
                 pygame.draw.rect(screen,self.colors[cellNum],cellRect)
+    """
+    reset function is used to reset the field.
+    If the player loses the game, this function will clear the field.
+    """
     def reset(self):
         for i in range(self.rows):
             for j in range(self.columns):
