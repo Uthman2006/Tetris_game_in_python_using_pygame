4250dfa931c2a1b3f08c0c7467eaa32de28ad3e6
uthmancan <uthmancan@uthmancan.uthmancan>
2025-02-13 16:14:14 +0100

    nan

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ insettings=False
 ingame=False
 run=True
 pygame.display.set_caption("Tetris")
-GAMEUPDATE=pygame.USEREVENT # this varibale is used to save the event which is the updateing the game. withouy this varibale game moves fast.
+GAMEUPDATE=pygame.USEREVENT # this variable is used to save the event that is user created. In this case it is the updating the game. without this variable game moves fast.
 pygame.time.set_timer(GAMEUPDATE,200)
 bg=pygame.image.load("../materials/pngwing.com.png")
 walls=pygame.image.load("../materials/walls.png")
